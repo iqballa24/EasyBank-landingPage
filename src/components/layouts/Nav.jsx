efd32c058f7c931712ffbf1c04a98756c3f00f4d
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-10-04 08:57:34 +0700

    set folder structure & created component nav

---
+++ src/components/layouts/Nav.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Logo from "../../assets/img/logo.svg";
+import { navMenu } from "../../constant/nav";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [widthSpan, setWidthSpan] = useState({ span1: 100, span3: 100 });
+
+  return (
+    <nav className="nav">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <NavLink to="/">
+            <img className="img-fluid logo" src={Logo} alt="logo" />
+          </NavLink>
+        </div>
+        <ul className={`nav-menus ${isOpen ? "show" : ""}`}>
+          {navMenu.map((item, index) => {
+            return (
+              <NavLink
+                key={index}
+                to={item.path}
+                className={({ isActive }) =>
+                  isActive ? "nav-menus-item active" : "nav-menus-item"
+                }
+              >
+                {item.name}
+              </NavLink>
+            );
+          })}
+        </ul>
+        <div
+          className="nav-hamburger"
+          onClick={() => {
+            document.body.classList.add("disabledScroll");
+            setIsOpen((prev) => !prev);
+            setWidthSpan((prev) =>
+              prev.span1 == "50"
+                ? { span1: 100, span3: 100 }
+                : { span1: 50, span3: 75 }
+            );
+          }}
+        >
+          <input type="checkbox" id="check" />
+          <span
+            className="nav-span"
+            style={{ width: `${widthSpan.span1}%` }}
+          ></span>
+          <span className="nav-span"></span>
+          <span
+            className="nav-span"
+            style={{ width: `${widthSpan.span3}%` }}
+          ></span>
+        </div>
+        <div className="nav-button">
+          <button className="btn btn-primary">Request Invite</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/constant/nav.jsx
@@ -0,0 +1,22 @@
+export const navMenu = [
+  {
+    name: "Home",
+    path: "/home",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Careers",
+    path: "/careers",
+  },
+];
