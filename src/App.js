a65cd81a211ca992db2b8ab43fc5b989cc7995dd
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-10-06 16:35:58 +0700

    Updated page not found

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
         <Route path="*" index element={<Home />} />
         <Route path="about" element={<NotFound />} />
         <Route path="contact" element={<NotFound />} />
+        <Route path="blogs" element={<NotFound />} />
+        <Route path="careers" element={<NotFound />} />
+        <Route path="support" element={<NotFound />} />
+        <Route path="privacy-police" element={<NotFound />} />
         <Route path="" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "../components/layouts";
+import ImgConstruction from "../assets/img/construction.svg";
 
 const NotFound = () => {
   return (
     <Layout>
-      <div>NotFound</div>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img
+          src={ImgConstruction}
+          alt="web under construction"
+          style={{
+            width: "100%",
+            maxWidth: "50rem",
+          }}
+        />
+      </div>
     </Layout>
   );
 };
